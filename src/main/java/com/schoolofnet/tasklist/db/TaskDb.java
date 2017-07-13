81708239ed35315061a1cc8bebc8739a37596166
brunocbarreto <brunocaldeirabarreto@gmail.com>
2017-07-13 14:26:03 -0300

    Upload dos arquivos do app Tasklist
    Foram enviados os arquivos da pasta src do projeto

---
+++ src/main/java/com/schoolofnet/tasklist/db/TaskDb.java
@@ -0,0 +1,11 @@
+package com.schoolofnet.tasklist.db;
+
+/**
+ * Created by bruno on 08/07/2017.
+ */
+
+public class TaskDb {
+
+    public static final String DB_NAME = "com.schoolofnet.tasklist.db";
+    public static final Integer DB_VERSION = 1;
+}
